d9bc322f9f5c8a762cb222320b95602dc7f1ea4d
LaneRamp <lerickson@liveramp.com>
2017-01-18 13:38:49 -0800

    Update auth.rb

--- lib/vault/api/sys/auth.rb
+++ lib/vault/api/sys/auth.rb
@@ -54,7 +54,7 @@ module Vault
     #
     # @return [true]
     def enable_auth(path, type, description = nil)
-      payload = { type: type }
+      payload = { :type => type }
       payload[:description] = description if !description.nil?
 
       client.post("/v1/sys/auth/#{encode_path(path)}", JSON.fast_generate(payload))
